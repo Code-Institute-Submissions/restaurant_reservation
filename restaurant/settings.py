c1b4184f78a337a748c14aa1890604d51de60cb9
mariaarnesson <maria_arnesson@outlook.com>
2023-02-01 08:36:48 +0000

    Initial commit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -48,6 +48,14 @@ INSTALLED_APPS = [
     'reservation',
 ]
 
+SITE_ID = 1
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
