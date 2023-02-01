4c788e299c329fd33b4b3f1a74b29dd4807f90de
mariaarnesson <maria_arnesson@outlook.com>
2023-02-01 11:02:51 +0000

    Update readmefile

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'reservation',
+    'members',
 ]
 
 SITE_ID = 1
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('reservation.urls')), 
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('user', include("members.urls")),
 ]
